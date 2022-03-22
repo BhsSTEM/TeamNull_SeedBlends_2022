6a5af158469d0c82634839b82cb6ff4588c7938c
SalarC123 <salarwcheema@gmail.com>
2022-03-22 01:41:56 -0500

    create intents to open up tracking, field, and documentations page

--- app/src/main/java/com/example/teamnull_seedblends_2022/MainActivity.java
+++ app/src/main/java/com/example/teamnull_seedblends_2022/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.teamnull_seedblends_2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity
@@ -14,6 +16,21 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void launchTrackingPage(View view) {
+        Intent intent = new Intent(this, Tracking.class);
+        startActivity(intent);
+    }
+
+    public void launchFieldPage(View view) {
+        Intent intent = new Intent(this, Field.class);
+        startActivity(intent);
+    }
+
+    public void launchDocumentationPage(View view) {
+        Intent intent = new Intent(this, Documentation.class);
+        startActivity(intent);
     }
 }
