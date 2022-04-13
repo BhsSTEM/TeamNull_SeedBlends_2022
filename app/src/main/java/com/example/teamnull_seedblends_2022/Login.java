7f812df83b7285e9dbd31be2e331ec6231ce7fda
senpeii033 <98488178+senpeii033@users.noreply.github.com>
2022-04-13 13:45:57 -0500

    Firebase Register and Login screen is functional :))))))

--- app/src/main/java/com/example/teamnull_seedblends_2022/Login.java
+++ app/src/main/java/com/example/teamnull_seedblends_2022/Login.java
@@ -1,21 +1,36 @@
 package com.example.teamnull_seedblends_2022;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.accounts.Account;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.view.textservice.TextInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener{
 
+    private TextView register;
+    private EditText signInEmail, signInPassword;
+    private Button logInButton;
+    private ProgressBar signInProgressBar;
+    // This is got the google fire base login System
+    private FirebaseAuth mAuth;
+    //This is got the google fire base login System
 
 
 
@@ -23,23 +38,78 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        register = (TextView) findViewById(R.id.register);
+            register.setOnClickListener(this);
+// DONT TOUCH THIS
+
+        logInButton = (Button) findViewById(R.id.logInButton);
+            logInButton.setOnClickListener(this);
+                    // This is for the login button
+
+        signInEmail = (EditText) findViewById(R.id.signInEmail);
+            signInPassword = (EditText) findViewById(R.id.signInPassword);
+                    // This is for the Password and Email
+        signInProgressBar = (ProgressBar) findViewById(R.id.signInProgressBar);
+
+            mAuth = FirebaseAuth.getInstance();
+            //FIRE BASE AUTH DO NOT TOUCH
     }
 
-
-
-    public void launchMainScreen(View view){
-    Intent intent = new Intent(this, MainActivity.class);
-    startActivity(intent);
-}
-
-public void launchSignUpScreen(View view){
-        Intent intent = new Intent(this, signUpScreen.class);
+    public void launchMainScreen(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-}
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.register:
+                startActivity(new Intent(this, signUpScreen.class));
+                break;
 
+            case R.id.logInButton:
+                userLogin();
+                break;
+        }
+    }
 
+    private void userLogin() {
+        // This is our path method
+        String email = signInEmail.getText().toString().trim();
+        String password = signInPassword.getText().toString().trim();
+        // Variables for the values
 
+        if(email.isEmpty()){
+            signInEmail.setError("Please enter your email");
+            signInEmail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            signInEmail.setError("Please enter a valid Email!");
+            signInEmail.requestFocus();
+            return;
+        }
 
+        if(password.isEmpty() || password.length() < 6){
+            signInPassword.setError("Please enter your passsword!");
+            signInPassword.requestFocus();
+            return;
+        }
 
+        signInProgressBar.setVisibility(View.VISIBLE);
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    // This is where we Redirect to user profile
+                    startActivity(new Intent(Login.this, MainActivity.class));
+
+                }else{
+                    Toast.makeText(Login.this, "Failed to login! Please try again!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
 }
--- app/src/main/java/com/example/teamnull_seedblends_2022/signUpScreen.java
+++ app/src/main/java/com/example/teamnull_seedblends_2022/signUpScreen.java
@@ -1,21 +1,129 @@
 package com.example.teamnull_seedblends_2022;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class signUpScreen extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
+
+public class signUpScreen extends AppCompatActivity implements View.OnClickListener{
+
+    private FirebaseAuth mAuth;
+    private TextView banner;
+    private EditText registerName, registerPassword, registerEmail;
+    private Button registerButton;
+    private ProgressBar registerProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_screen);
+        mAuth = FirebaseAuth.getInstance();
+
+        banner = (TextView) findViewById(R.id.banner);
+        banner.setOnClickListener(this);
+        // This is for the "Register" Banner for the user to go back to the home screen
+        registerButton = (Button) findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(this);
+        // This is for the button
+
+        registerName = (EditText) findViewById(R.id.registerName);
+        registerPassword = (EditText) findViewById(R.id.registerPassword);
+        registerEmail = (EditText) findViewById(R.id.registerEmail);
+        registerProgressBar = (ProgressBar) findViewById(R.id.registerProgressBar);
+        // This is for all the other variable that we have
+
+
+
+
     }
     public void launchMainScreen(View view){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.banner:
+                startActivity(new Intent(this, Login.class));
+                break;
+            case R.id.registerButton:
+               registerButton();
+               break;
+        }
+    }
+
+    private void registerButton() {
+        String email = registerEmail.getText().toString().trim();
+        String name = registerName.getText().toString().trim();
+        String password = registerPassword.getText().toString().trim();
+
+        if(email.isEmpty()){
+            registerEmail.setError("Please enter a email!");
+            registerEmail.requestFocus();
+            return;
+        }
+        if (name.isEmpty()){
+            registerName.setError("Please enter your name!");
+            registerName.requestFocus();
+            return;
+        }
+        if(password.isEmpty() || password.length() < 6){
+            registerPassword.setError("Please enter a valid password!");
+            registerName.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            registerEmail.setError("Please provide a valid Email!");
+            registerEmail.requestFocus();
+            return;
+        }
+        registerProgressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            User user = new User(name, password, email);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(signUpScreen.this, "User has been registered!", Toast.LENGTH_SHORT).show();
+                                        registerProgressBar.setVisibility(View.VISIBLE);
+                                    } else {
+                                        Toast.makeText(signUpScreen.this, "Failed to register User!", Toast.LENGTH_SHORT).show();
+                                        registerProgressBar.setVisibility(View.GONE);
+                                    }
+                                }
+                            });
+                        }else {
+                            Toast.makeText(signUpScreen.this, "Failed to register User!", Toast.LENGTH_SHORT).show();
+                            registerProgressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+
+    }
 }
